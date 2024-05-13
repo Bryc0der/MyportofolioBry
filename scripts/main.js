17333ea9daebbfd6caa186848e173d341ee5030f
Brycoder <vcrwebdev@gmail.com>
2024-05-13 21:02:55 +0700

    first commit

---
+++ scripts/main.js
@@ -0,0 +1,20 @@
+// active toggle & responsive navbar
+const navSlide = () => {
+    const burger = document.querySelector(".burger");
+    const navLists = document.querySelector("nav");
+
+    burger.addEventListener("click", () =>{
+        navLists.classList.toggle("nav-active");
+        burger.classList.toggle("toggle-burger");
+    });
+};
+
+navSlide();
+
+
+// Clear form before unload tick
+window.onbeforeunload = () => {
+    for (const form of document.getElementsByTagName("form")){
+        form.reset();
+    }
+}
